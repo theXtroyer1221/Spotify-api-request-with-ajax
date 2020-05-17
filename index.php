1bca953af4ca68d2e307ef32b8dc97b60968d984
theXtroyer1221 <theXtroyer1221@users.noreply.github.com>
2020-05-17 12:53:01 +0200

    started with the base of the project, starting t o play with the api now

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php include("base.php")?>
+<header class="mb-2">
+    <div class="jumbotron">
+        <div class="container d-flex justify-content-center py-5">
+            <h1>Spotify API <small>Helo</small></h1>
+        </div>
+    </div>
+</header>
+<section mb-2>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h2>Heading</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+                    mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
+                    mollis euismod. Donec sed odio dui. </p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div>
+            <div class="col-md-4">
+                <h2>Heading</h2>
+                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
+                    mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
+                    mollis euismod. Donec sed odio dui. </p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div>
+            <div class="col-md-4">
+                <h2>Heading</h2>
+                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula
+                    porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh,
+                    ut fermentum massa justo sit amet risus.</p>
+                <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div>
+        </div>
+        <hr>
+    </div>
+</section>
+<?php include("base_bottom.php")?>
